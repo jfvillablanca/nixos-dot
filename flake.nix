3cec810c63cb7aa75dc0fca3e4a03dab2e8d0c37
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-12 17:26:50 +0800

    Refactor with let in

--- flake.nix
+++ flake.nix
@@ -9,12 +9,19 @@
     };
   };
 
-  # outputs = { self, nixpkgs, ... }: 
   outputs = inputs@{ nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+    };
+    lib = nixpkgs.lib;
+  in
     {
       nixosConfigurations = {
-        jmfv = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
+        jmfv = lib.nixosSystem {
+            inherit system;
           modules = [
               ./configuration.nix
               home-manager.nixosModules.home-manager 
