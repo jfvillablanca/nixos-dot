7563ebe50abe1dcb502682f88afa944c5019b10c
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-15 19:51:40 +0800

    Add starship settings

--- home.nix
+++ home.nix
@@ -130,6 +130,97 @@
 
     starship = {
         enable = true;
+        enableZshIntegration = true;
+        settings = 
+        ''
+        {
+            format = """
+            $username\
+            $hostname\
+            $directory\
+            $git_branch\
+            $git_state\
+            $git_status\
+            $git_metrics\
+            $fill\
+            $nodejs\
+            $rust\
+            $jobs\
+            $memory_usage\
+            $line_break\
+            $character"""
+
+            add_newline = false
+
+            palette = 'kanagawa'
+
+            [palettes.kanagawa]
+            oldwhite = '#c8c093'
+            roninyellow = '#ff9e3b'
+            autumngreen = '#76946A'
+            crystalblue = '#7E9CD8'
+            surimiorange = '#FFA066'
+
+            [username]
+            style_user = 'autumngreen bold'
+            style_root = 'black bold'
+            format = '[$user]($style) '
+            disabled = false
+            show_always = true
+
+            [hostname]
+            ssh_only = false
+            format = '[$ssh_symbol](bold cyan)[$hostname](bold roninyellow) '
+            trim_at = '.companyname.com'
+            disabled = false
+
+            [directory]
+            style = "crystalblue"
+            read_only = " "
+            truncation_length = 1
+            truncate_to_repo = false
+
+            [git_branch]
+            symbol = " "
+            format = "[$symbol$branch]($style) "
+            style = "surimiorange"
+
+            [git_state]
+            format = '\([$state( $progress_current/$progress_total)]($style)\) '
+            style = "bright-black"
+
+            [git_status]
+            format = '([\[$all_status$ahead_behind\]]($style) )'
+            style = "cyan"
+
+            [git_metrics]
+            disabled = false
+
+            [fill]
+            symbol = " "
+
+            [nodejs]
+            format = "[$symbol($version )]($style)"
+            disabled = true
+
+            [rust]
+            symbol = " "
+
+            [jobs]
+            symbol = ""
+            style = "bold red"
+            number_threshold = 1
+            format = "[$symbol]($style)"
+
+            [memory_usage]
+            symbol = " "
+
+            [character]
+            success_symbol = "[❯](purple)"
+            error_symbol = "[❯](red)"
+            vicmd_symbol = "[❮](green)"
+        }
+        '';
     };
 
     alacritty = {
