07354a2a63988f0281b2974216446a7335ba81c0
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-15 19:23:39 +0800

    Enable bat

--- home.nix
+++ home.nix
@@ -58,6 +58,18 @@
         enableAliases = true;
     };
 
+    bat = {
+        enable = true;
+        themes = {
+          dracula = builtins.readFile (pkgs.fetchFromGitHub {
+            owner = "dracula";
+            repo = "sublime"; # Bat uses sublime syntax for its themes
+            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+          } + "/Dracula.tmTheme");
+        };
+    };
+
     starship = {
         enable = true;
     };
