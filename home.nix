e4dfd7150bd829f272ab9e158c48cd192d0da800
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-16 22:08:05 +0800

    Dry run of importing xmonad config

--- home.nix
+++ home.nix
@@ -1,4 +1,7 @@
 { pkgs, ... }:
+let
+    xmonadConfig = builtins.readFile ~/.config/nixos/modules/wm/xmonad/xmonad.hs;
+in
 {
     home = {
         username = "jmfv";
@@ -425,44 +428,45 @@
             xmonad = {
                 enable = true;
                 enableContribAndExtras = true;
-                config = pkgs.writeText "xmonad.hs" ''
-                import XMonad
+                config = xmonadConfig;
+                # config = pkgs.writeText "xmonad.hs" ''
+                # import XMonad
 
-                import XMonad.Util.EZConfig
-                import XMonad.Util.Ungrab
-                import XMonad.Layout.ThreeColumns
-                import XMonad.Hooks.EwmhDesktops
+                # import XMonad.Util.EZConfig
+                # import XMonad.Util.Ungrab
+                # import XMonad.Layout.ThreeColumns
+                # import XMonad.Hooks.EwmhDesktops
 
-                main :: IO ()
-                main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+                # main :: IO ()
+                # main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
 
-                myConfig = def
-                    { terminal    = "alacritty"
-                    , modMask     = mod4Mask
-                    , borderWidth = 2
-                    , layoutHook  = myLayout                -- Custom layout
-                    }
-                    `additionalKeysP`
-                    [ ("M-C-s", unGrab *> spawn "scrot -s")
-                    ]
+                # myConfig = def
+                #     { terminal    = "alacritty"
+                #     , modMask     = mod4Mask
+                #     , borderWidth = 2
+                #     , layoutHook  = myLayout                -- Custom layout
+                #     }
+                #     `additionalKeysP`
+                #     [ ("M-C-s", unGrab *> spawn "scrot -s")
+                #     ]
 
-                myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
-                  where
-                    threeCol = ThreeColMid nmaster delta ratio
-                    tiled    = Tall nmaster delta ratio
-                    nmaster  = 1      -- Default number of windows in the master pane
-                    ratio    = 1/2    -- Default proportion of screen occupied by master pane
-                    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+                # myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+                #   where
+                #     threeCol = ThreeColMid nmaster delta ratio
+                #     tiled    = Tall nmaster delta ratio
+                #     nmaster  = 1      -- Default number of windows in the master pane
+                #     ratio    = 1/2    -- Default proportion of screen occupied by master pane
+                #     delta    = 3/100  -- Percent of screen to increment by when resizing panes
 
-                -- import XMonad.Util.EZConfig(additionalKeys)
+                # -- import XMonad.Util.EZConfig(additionalKeys)
 
-                -- main = xmonad $ defaultConfig `additionalKeys`
-                --        [ ((mod4Mask, xK_Left),  sendMessage $ Go L)
-                --        , ((mod4Mask, xK_Down),  sendMessage $ Go D)
-                --        , ((mod4Mask, xK_Up),    sendMessage $ Go U)
-                --        , ((mod4Mask, xK_Right), sendMessage $ Go R)
-                --        ]
-                '';
+                # -- main = xmonad $ defaultConfig `additionalKeys`
+                # --        [ ((mod4Mask, xK_Left),  sendMessage $ Go L)
+                # --        , ((mod4Mask, xK_Down),  sendMessage $ Go D)
+                # --        , ((mod4Mask, xK_Up),    sendMessage $ Go U)
+                # --        , ((mod4Mask, xK_Right), sendMessage $ Go R)
+                # --        ]
+                # '';
             };
         };
     };
