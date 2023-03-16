7ade56e562a918a8db8aa20bd3b1c9e707cc1b16
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-16 14:51:20 +0800

    Add zellij to programs

--- home.nix
+++ home.nix
@@ -68,6 +68,33 @@
             };
         };
     };
+
+    zellij = {
+        enable = true;
+        settings = {
+            default_shell = "zsh";
+            ui = {
+                pane_frames.rounded_corners = true;
+            };
+            themes = {
+                kanagawa = {
+                    fg = "#DCD7BA";
+                    bg = "#2A2A37";
+                    red = "#C34043";
+                    green = "#76946A";
+                    yellow = "#FF9E3B";
+                    blue = "#7E9CD8";
+                    magenta = "#957FB8";
+                    orange = "#FFA066";
+                    cyan = "#7FB4CA";
+                    black = "#2A2A37";
+                    white = "#DCD7BA";
+                };
+            };
+            theme = "kanagawa";
+        };
+    };
+
     zsh = {
         enable = true;
         enableAutosuggestions = true;
