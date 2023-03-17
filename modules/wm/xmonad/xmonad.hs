202b51c839f747a56279d1869f4993e4697de471
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-17 08:52:33 +0800

    Unignore modules folder

---
+++ modules/wm/xmonad/xmonad.hs
@@ -0,0 +1,36 @@
+import XMonad
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Ungrab
+import XMonad.Layout.ThreeColumns
+import XMonad.Hooks.EwmhDesktops
+
+main :: IO ()
+main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+
+myConfig = def
+    { terminal    = "alacritty"
+    , modMask     = mod4Mask
+    , borderWidth = 2
+    , layoutHook  = myLayout                -- Custom layout
+    }
+    `additionalKeysP`
+    [ ("M-C-s", unGrab *> spawn "scrot -s")
+    ]
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+-- import XMonad.Util.EZConfig(additionalKeys)
+
+-- main = xmonad $ defaultConfig `additionalKeys`
+--        [ ((mod4Mask, xK_Left),  sendMessage $ Go L)
+--        , ((mod4Mask, xK_Down),  sendMessage $ Go D)
+--        , ((mod4Mask, xK_Up),    sendMessage $ Go U)
+--        , ((mod4Mask, xK_Right), sendMessage $ Go R)
+--        ]
