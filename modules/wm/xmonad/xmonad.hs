b56e07cc1ca52043f257244ceb6614a0ebf5fd97
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-17 17:57:26 +0800

    Let's try to pretty print :)

--- modules/wm/xmonad/xmonad.hs
+++ modules/wm/xmonad/xmonad.hs
@@ -2,6 +2,7 @@ import XMonad
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Util.Loggers
 
 import XMonad.Layout.ThreeColumns
 
@@ -11,7 +12,14 @@ import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 
 main :: IO ()
-main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
+main = xmonad
+     . ewmhFullscreen
+     . ewmh
+     . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
+     $ myConfig
+
+xmobarProp config =
+  withEasySB (statusBarProp "xmobar" (pure xmobarPP)) toggleStrutsKey config
 
 myConfig = def
     { terminal    = "alacritty"
@@ -30,6 +38,34 @@ myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
     nmaster  = 1      -- Default number of windows in the master pane
     ratio    = 1/2    -- Default proportion of screen occupied by master pane
     delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+myXmobarPP :: PP
+myXmobarPP = def
+        { ppSep             = magenta " • "
+        , ppTitleSanitize   = xmobarStrip
+        , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+        , ppHidden          = white . wrap " " ""
+        , ppHiddenNoWindows = lowWhite . wrap " " ""
+        , ppUrgent          = red . wrap (yellow "!") (yellow "!")
+        , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
+        , ppExtras          = [logTitles formatFocused formatUnfocused]
+        }
+      where
+        formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
+        formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+
+        -- | Windows should have *some* title, which should not not exceed a
+        -- sane length.
+        ppWindow :: String -> String
+        ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+
+        blue, lowWhite, magenta, red, white, yellow :: String -> String
+        magenta  = xmobarColor "#ff79c6" ""
+        blue     = xmobarColor "#bd93f9" ""
+        white    = xmobarColor "#f8f8f2" ""
+        yellow   = xmobarColor "#f1fa8c" ""
+        red      = xmobarColor "#ff5555" ""
+        lowWhite = xmobarColor "#bbbbbb" ""
 
 -- import XMonad.Util.EZConfig(additionalKeys)
 
