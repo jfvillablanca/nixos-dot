fff6d9d7d9764d826d487fbbd9ccb34f2b584df6
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-17 09:05:40 +0800

    Chore: cleanup of unused bindings

--- home.nix
+++ home.nix
@@ -1,12 +1,4 @@
 { pkgs, ... }:
-let
-    user = "jmfv";
-    homeDirectory = "/home/${user}";
-    configHome = "${homeDirectory}/nixos/.config/";
-    flakeModules = "${configHome}/nixos/modules";
-
-    xmonadConfig = "${flakeModules}/wm/xmonad/xmonad.hs";
-in
 {
     home = {
         username = "jmfv";
@@ -184,6 +176,7 @@ in
     bat = {
         enable = true;
         config = {
+            # FIXME: dracula is not recognized as a theme
             theme = "dracula";
         };
         themes = {
@@ -435,49 +428,9 @@ in
             xmonad = {
                 enable = true;
                 enableContribAndExtras = true;
-                # config = xmonadConfig;
                 config = pkgs.writeText "xmonad.hs" ''
-                ${builtins.readFile ./modules/wm/xmonad/xmonad.hs}
+                    ${builtins.readFile ./modules/wm/xmonad/xmonad.hs}
                 '';
-
-                # config = pkgs.writeText "xmonad.hs" ''
-                # import XMonad
-
-                # import XMonad.Util.EZConfig
-                # import XMonad.Util.Ungrab
-                # import XMonad.Layout.ThreeColumns
-                # import XMonad.Hooks.EwmhDesktops
-
-                # main :: IO ()
-                # main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
-
-                # myConfig = def
-                #     { terminal    = "alacritty"
-                #     , modMask     = mod4Mask
-                #     , borderWidth = 2
-                #     , layoutHook  = myLayout                -- Custom layout
-                #     }
-                #     `additionalKeysP`
-                #     [ ("M-C-s", unGrab *> spawn "scrot -s")
-                #     ]
-
-                # myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
-                #   where
-                #     threeCol = ThreeColMid nmaster delta ratio
-                #     tiled    = Tall nmaster delta ratio
-                #     nmaster  = 1      -- Default number of windows in the master pane
-                #     ratio    = 1/2    -- Default proportion of screen occupied by master pane
-                #     delta    = 3/100  -- Percent of screen to increment by when resizing panes
-
-                # -- import XMonad.Util.EZConfig(additionalKeys)
-
-                # -- main = xmonad $ defaultConfig `additionalKeys`
-                # --        [ ((mod4Mask, xK_Left),  sendMessage $ Go L)
-                # --        , ((mod4Mask, xK_Down),  sendMessage $ Go D)
-                # --        , ((mod4Mask, xK_Up),    sendMessage $ Go U)
-                # --        , ((mod4Mask, xK_Right), sendMessage $ Go R)
-                # --        ]
-                # '';
             };
         };
     };
