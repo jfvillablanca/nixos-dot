2f8636ef3d369573b125e75055f566b75d28b670
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-18 15:55:27 +0800

    Try out some sample i3status-rs config

--- modules/wm/i3status-rust/default.nix
+++ modules/wm/i3status-rust/default.nix
@@ -3,6 +3,51 @@
     programs = {
         i3status-rust = {
             enable = true;
+            bars = {
+                top = {
+                  blocks = [
+                    {
+                       block = "disk_space";
+                       path = "/";
+                       info_type = "available";
+                       interval = 60;
+                       warning = 20.0;
+                       alert = 10.0;
+                     }
+                     {
+                       block = "memory";
+                       format_mem = " $icon $mem_used_percents ";
+                       format_swap = " $icon $swap_used_percents ";
+                     }
+                     {
+                       block = "cpu";
+                       interval = 1;
+                     }
+                     {
+                       block = "load";
+                       interval = 1;
+                       format = " $icon $1m ";
+                     }
+                     { block = "sound"; }
+                     {
+                       block = "time";
+                       interval = 60;
+                       format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+                     }
+                  ];
+                  settings = {
+                    theme =  {
+                      theme = "solarized-dark";
+                      overrides = {
+                        idle_bg = "#123456";
+                        idle_fg = "#abcdef";
+                      };
+                    };
+                  };
+                  icons = "awesome5";
+                  theme = "gruvbox-dark";
+                };
+            };
         };
     };
 }
