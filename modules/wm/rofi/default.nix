11a20f343333a3b0436fb4b287e38503c3903ed5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-18 19:03:32 +0800

    Add rofi options

--- modules/wm/rofi/default.nix
+++ modules/wm/rofi/default.nix
@@ -4,6 +4,9 @@
         rofi = {
             enable = true;
             terminal = "${pkgs.alacritty}/bin/alacritty";
+            configPath = "$XDG_CONFIG_HOME/rofi/config.rasi";
+            font = "JetBrainsMono Nerd Font 13";
+            location = "center";
         };
     };
 }
