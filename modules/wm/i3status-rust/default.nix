91274abc6508018cd5500e451aecbb77efc2bbde
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-18 18:05:02 +0800

    Add disk space block

--- modules/wm/i3status-rust/default.nix
+++ modules/wm/i3status-rust/default.nix
@@ -12,6 +12,19 @@
                   interval = 60;
                   format = "%a %d/%m %k:%M";
                 }
+                {
+                    block = "disk_space";
+                    click = {
+                        button = "left";
+                        update = true;
+                    };
+                    info_type = "available";
+                    alert_unit = "GB";
+                    alert = 10.0;
+                    warning = 15.0;
+                    format = " $icon $available ";
+                    format_alt = " $icon $available / $total ";
+                }
                 ];
             };
         };
