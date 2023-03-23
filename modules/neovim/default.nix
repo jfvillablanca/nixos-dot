35bc5cba6a7757823a553564dd17ff74f249c72f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-23 08:54:02 +0800

    Gigantic rewrite of nvim config, wip baybeh

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,31 +1,33 @@
 { config, pkgs, ... }:
-let
-    mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
-in
 {
-    home.file.".config/nvim" = {
-        source = mkOutOfStoreSymlink ./nvim;
-        recursive = true;
-    };
+    programs.neovim = {
+        enable = true;
+        package = pkgs.neovim-nightly;
+        defaultEditor = true;
+        vimAlias = true;
+        withNodeJs = true;
+        withPython3 = true;
+        withRuby = true;
 
-    programs = {
-        neovim = {
-            enable = true;
-            package = pkgs.neovim-nightly;
-            defaultEditor = true;
-            vimAlias = true;
-            withNodeJs = true;
-            withPython3 = true;
+        extraLuaConfig = builtins.readFile ./nvim/lua/user/options.lua;
 
-            extraPackages = with pkgs; [
-                shfmt
-            ];
+        plugins = with pkgs.vimPlugins; [
+            plenary-nvim
+            {
+                plugin = impatient-nvim;
+                type = "lua";
+                config = ''
+                    local status_ok, impatient = pcall(require,  "impatient")
+                    if not status_ok then
+                        return
+                    end
+                    impatient.enable_profile()
+                '';
+            }
 
-            plugins = with pkgs.vimPlugins; [
-                nvim-lspconfig
-                mason-lspconfig-nvim
-                mason-nvim
-            ];
-        };
+            nvim-lspconfig
+            mason-lspconfig-nvim
+            mason-nvim
+        ];
     };
 }
