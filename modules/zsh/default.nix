372ca22698f06e31d5dd9bca2011077e88660853
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-25 11:35:08 +0800

    Remove alias

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -8,7 +8,6 @@
             enableSyntaxHighlighting = true;
             shellAliases = {
                 ".." = "cd ..";
-                "mkdir" = "mkdir -r";
                 "use" = "nix-shell -p";
                 "nixs" = ''
                 manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
