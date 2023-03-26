0c66e75742d3a3729e689aec0d03676a86a1d1fd
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-26 11:50:15 +0800

    bat theme was just a typo

--- modules/bat/default.nix
+++ modules/bat/default.nix
@@ -4,16 +4,7 @@
         bat = {
             enable = true;
             config = {
-                # FIXME: dracula is not recognized as a theme
-                theme = "dracula";
-            };
-            themes = {
-              dracula = builtins.readFile (pkgs.fetchFromGitHub {
-                owner = "dracula";
-                repo = "sublime"; # Bat uses sublime syntax for its themes
-                rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
-                sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
-              } + "/Dracula.tmTheme");
+                theme = "Dracula";
             };
         };
     };
