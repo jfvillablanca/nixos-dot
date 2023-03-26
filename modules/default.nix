ca3b893b70bb1a737e7c9b5d6c9cd6581198a8f4
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-26 13:31:02 +0800

    Separate imports for x and wayland

--- modules/default.nix
+++ modules/default.nix
@@ -1,25 +1,38 @@
-{ config, pkgs, lib, ... }:
-{
-    imports = [
-        ./alacritty 
-        ({ ... }: import ./neovim { inherit config pkgs; }) 
-        ./starship 
-        ./direnv 
-        ./zoxide 
-        ./zellij 
-        ./exa 
-        ./zsh 
-        ./bash
-        ./gitui 
-        ./git 
-        ./bat 
+{ config, pkgs, lib, isWayland, ... }:
+let
+  modules = [
+    ./alacritty
+    ({ ... }: import ./neovim { inherit config pkgs; })
+    ./starship
+    ./direnv
+    ./zoxide
+    ./zellij
+    ./exa
+    ./zsh
+    ./bash
+    ./gitui
+    ./git
+    ./bat
 
-        # Window Manager
-        ./wm/autorandr 
-        ./wm/polybar 
-        ./wm/i3status-rust 
-        ./wm/rofi 
-        ./wm/xmonad 
-        ./wm/i3 
-    ];
+    # Window Manager
+    ./wm/rofi
+  ];
+
+  x11Modules = [
+    # ./wm/xmonad 
+    ./wm/autorandr
+    ./wm/polybar
+    ./wm/i3status-rust
+    ./wm/i3
+  ];
+
+  waylandModules = [
+  ];
+in
+{
+  imports = modules ++
+    (if isWayland then
+      waylandModules
+    else
+      x11Modules);
 }
