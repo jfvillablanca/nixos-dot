95984e851fa71806de9805c0fdab4ce5d683f912
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-26 14:13:06 +0800

    Move picom to own module

--- home.nix
+++ home.nix
@@ -50,23 +50,6 @@ in
   };
 
   services = {
-    picom = {
-      enable = true;
-      activeOpacity = 0.95;
-      inactiveOpacity = 0.7;
-      backend = "xrender";
-
-      # NOTE: Don't know how to use glx in QEMU/KVM machine
-      # settings = {
-      #     wintypes = {
-      #         normal = { blur-background = true; };
-      #     };
-      #     blur = {
-      #         method = "dual_kawase";
-      #         strength = 2;
-      #     };
-      # };
-    };
   };
 
   xsession = {
