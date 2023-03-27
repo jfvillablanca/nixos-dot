e7e7b413a2860222cbd8d842dda07aabaceda1da
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-27 10:36:55 +0800

    set moz wayland env variable

--- modules/wm/wayland/default.nix
+++ modules/wm/wayland/default.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    wl-clipboard                    # Clipboard
-    sway-contrib.grimshot           # Screenshot utility
-  ];
-  programs = {
+  home = {
+    sessionVariables = {
+      MOZ_ENABLE_WAYLAND = "1";
+    };
+    packages = with pkgs; [
+      wl-clipboard # Clipboard
+      sway-contrib.grimshot # Screenshot utility
+    ];
   };
+  programs = { };
 }
