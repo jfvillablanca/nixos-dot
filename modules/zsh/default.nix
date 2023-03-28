57ce3934c5209babb9009829ce3085db275f7084
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-28 20:00:49 +0800

    Add new zsh aliases

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,18 +1,20 @@
 { ... }:
 {
-    programs = {
-        zsh = {
-            enable = true;
-            enableAutosuggestions = true;
-            enableCompletion = true;
-            enableSyntaxHighlighting = true;
-            shellAliases = {
-                ".." = "cd ..";
-                "use" = "nix-shell -p";
-                "nixs" = ''
-                manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
-                '';
-            };
-        };
+  programs = {
+    zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      enableSyntaxHighlighting = true;
+      shellAliases = {
+        ".." = "cd ..";
+        "use" = "nix-shell -p";
+        "usep" = "nix-shell --pure -p";
+        "review" = " nix-shell -p nixpkgs-review --run 'nixpkgs-review rev HEAD'";
+        "nixs" = ''
+          manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
+        '';
+      };
     };
+  };
 }
