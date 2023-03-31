a7e0911ab0f982b8c6c345357a7c4adeb9b847db
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-03-31 08:34:58 +0800

    Format code to nixpkgs convention

--- flake.nix
+++ flake.nix
@@ -8,43 +8,43 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     neovim-nightly-overlay = {
-        url = "github:nix-community/neovim-nightly-overlay";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }@inputs: 
-  let
-    user = "jmfv";
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
+  outputs = { nixpkgs, home-manager, neovim-nightly-overlay, ... }@inputs:
+    let
+      user = "jmfv";
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
         overlays = [
-            neovim-nightly-overlay.overlay
+          neovim-nightly-overlay.overlay
         ];
-    };
-    lib = nixpkgs.lib;
-    isWayland = false;
-  in
+      };
+      lib = nixpkgs.lib;
+      isWayland = false;
+    in
     {
       nixosConfigurations = {
         ${user} = lib.nixosSystem {
-            inherit system;
+          inherit system;
           modules = [
             ({ config, ... }: import ./configuration.nix {
-            inherit config pkgs isWayland user;
+              inherit config pkgs isWayland user;
             })
-              home-manager.nixosModules.home-manager 
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.users.jmfv.imports = [ 
-                    ({ config, ... }: import ./home.nix {
-                    inherit config pkgs lib isWayland user;
-                    })
-                ];
-              } 
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.jmfv.imports = [
+                ({ config, ... }: import ./home.nix {
+                  inherit config pkgs lib isWayland user;
+                })
+              ];
+            }
           ];
         };
       };
