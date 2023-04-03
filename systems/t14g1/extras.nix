990a349613624edaa06ef554fe691fb0497a945e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-03 14:32:56 +0800

    Enable pipewire

--- systems/t14g1/extras.nix
+++ systems/t14g1/extras.nix
@@ -1,5 +1,16 @@
 { config, pkgs, ... }:
 {
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
   # Screen Brightness
   programs.light.enable = true;
   services.actkbd = {
