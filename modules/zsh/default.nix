b350adac3f2ce7801fe7f71966d41cc4ce7187fa
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-14 14:17:41 +0800

    Add metadata fetch zsh alias

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -11,6 +11,7 @@
         "use" = "nix-shell -p";
         "usep" = "nix-shell --pure -p";
         "review" = " nix-shell -p nixpkgs-review --run 'nixpkgs-review rev HEAD'";
+        "nixmeta" = "nix-shell -p nix-info --run 'nix-info -m'";
         "nixs" = ''
           manix "" | grep '^# ' | sed 's/^# \(.*\) (.*/\1/;s/ (.*//;s/^# //' | fzf --preview="manix '{}'" | xargs manix
         '';
