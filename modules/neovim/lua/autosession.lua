3a87e8e0981114fbbb4f73ec0be1e43f6c1f2553
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-04-30 17:59:40 +0800

    Add commented out feature (not yet available)

--- modules/neovim/lua/autosession.lua
+++ modules/neovim/lua/autosession.lua
@@ -3,14 +3,29 @@ if not status_ok then
 	return
 end
 
+-- local sessionlens = require("auto-session.session-lens")
+
 autosession.setup({
 	log_level = "error",
 	auto_session_suppress_dirs = { "~/", "~/Downloads", "/" },
+	auto_session_create_enabled = true,
+	auto_save_enabled = true,
+	auto_restore_enabled = true,
+	auto_session_use_git_branch = false,
+	auto_session_root_dir = vim.fn.stdpath("data") .. "/sessions/",
+	-- session_lens = {
+	--	theme_conf = { border = true },
+	--	previewer = true,
+	-- },
 	cwd_change_handling = {
-		restore_upcoming_session = true, -- already the default, no need to specify like this, only here as an example
-		pre_cwd_changed_hook = nil, -- already the default, no need to specify like this, only here as an example
-		post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-			require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-		end,
+		restore_upcoming_session = true,
+		pre_cwd_changed_hook = nil,
+		-- post_cwd_changed_hook = function()
+		-- 	require("lualine").refresh()
+		-- end,
 	},
 })
+
+-- vim.keymap.set("n", "<C-s>", sessionlens.search_session, {
+--	noremap = true,
+-- })
