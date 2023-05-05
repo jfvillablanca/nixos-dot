3e24775a683877924905f17650aa2195d67473f7
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-05 11:58:21 +0800

    Set picom settings for specific wintypes

--- modules/wm/x11/picom/default.nix
+++ modules/wm/x11/picom/default.nix
@@ -11,6 +11,23 @@
       settings = {
           wintypes = {
               normal = { blur-background = true; };
+              tooltip = {  
+                  fade = false; 
+                  shadow = true; 
+                  opacity = 1.0; 
+                  focus = true; 
+                  full-shadow = false; 
+              }; 
+              dock = { shadow = false; };
+              dnd = { shadow = false; };
+              popup_menu = { 
+                  opacity = 1.0; 
+                  fade = false; 
+              };
+              dropdown_menu = { 
+                  opacity = 1.0; 
+                  fade = false; 
+              }; 
           };
           blur = {
               method = "dual_kawase";
