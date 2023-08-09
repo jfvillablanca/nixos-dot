86d6eefbba6b9b010393b1c583feb093f6626658
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-08-09 17:53:02 +0800

    Set TLP default mode to BAT

--- systems/t14g1/configuration.nix
+++ systems/t14g1/configuration.nix
@@ -139,6 +139,8 @@
       # Values for "unplugged all the time"
       # START_CHARGE_THRESH_BAT0 = 85;
       # STOP_CHARGE_THRESH_BAT0 = 90;
+      TLP_DEFAULT_MODE = "BAT";
+      TLP_PERSISTENT_DEFAULT = 1;
     };
   };
 
