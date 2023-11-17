556db799a6367f5db7a2812a61e6b17fc3c02d37
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-11-17 19:12:30 +0800

    change starship prompt string

--- modules/starship/default.nix
+++ modules/starship/default.nix
@@ -1,128 +1,196 @@
 { lib, ... }:
 {
-    programs = {
+  programs = {
     starship = {
-        enable = true;
-        enableZshIntegration = true;
-        enableBashIntegration = true;
-        settings = 
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      settings =
         {
-            format = lib.concatStrings [
-            "$username"
-            "$hostname"
+          format = lib.concatStrings [
+            # "$username"
+            # "$hostname"
+            "$localip"
+            # "$shlvl"
+            "$singularity"
+            "$kubernetes"
             "$directory"
-            "$shlvl"
+            "$vcsh"
+            "$fossil_branch"
             "$git_branch"
+            "$git_commit"
             "$git_state"
-            "$git_status"
             "$git_metrics"
-            "$nix_shell"
-            "$fill"
+            "$git_status"
+            "$hg_branch"
+            "$pijul_channel"
+            "$docker_context"
+            "$package"
+            "$c"
+            "$cmake"
+            "$cobol"
+            "$daml"
+            "$dart"
+            "$deno"
+            "$dotnet"
+            "$elixir"
+            "$elm"
+            "$erlang"
+            "$fennel"
+            "$golang"
+            "$guix_shell"
+            "$haskell"
+            "$haxe"
+            "$helm"
+            "$java"
+            "$julia"
+            "$kotlin"
+            "$gradle"
+            "$lua"
+            "$nim"
             "$nodejs"
+            "$ocaml"
+            "$opa"
+            "$perl"
+            "$php"
+            "$pulumi"
+            "$purescript"
+            "$python"
+            "$raku"
+            "$rlang"
+            "$red"
+            "$ruby"
             "$rust"
-            "$jobs"
+            "$scala"
+            "$solidity"
+            "$swift"
+            "$terraform"
+            "$vlang"
+            "$vagrant"
+            "$zig"
+            "$buf"
+            "$nix_shell"
+            "$conda"
+            "$meson"
+            "$spack"
             "$memory_usage"
+            "$aws"
+            "$gcloud"
+            "$openstack"
+            "$azure"
+            "$env_var"
+            "$crystal"
+            "$custom"
+            "$sudo"
+            # "$cmd_duration"
             "$line_break"
+            "$jobs"
+            "$battery"
+            "$time"
+            "$status"
+            "$os"
+            "$container"
+            "$shell"
             "$character"
-            ];
+          ];
 
-            add_newline = false;
+          add_newline = false;
 
-            palette = "kanagawa";
+          palette = "kanagawa";
 
-            palettes.kanagawa = {
-                oldwhite = "#c8c093";
-                roninyellow = "#ff9e3b";
-                autumngreen = "#76946A";
-                crystalblue = "#7E9CD8";
-                surimiorange = "#FFA066";
-                samuraired = "#E82424";
-                autumnred = "#C34043";
-            };
+          palettes.kanagawa = {
+            oldwhite = "#c8c093";
+            roninyellow = "#ff9e3b";
+            autumngreen = "#76946A";
+            crystalblue = "#7E9CD8";
+            surimiorange = "#FFA066";
+            samuraired = "#E82424";
+            autumnred = "#C34043";
+          };
 
-            username = {
-                style_user = "autumngreen bold";
-                style_root = "black bold";
-                format = "[$user]($style) ";
-                disabled = false;
-                show_always = true;
-            };
+          username = {
+            style_user = "autumngreen bold";
+            style_root = "black bold";
+            format = "[$user]($style) ";
+            disabled = false;
+            show_always = true;
+          };
 
-            hostname = {
-                ssh_only = false;
-                format = "[$ssh_symbol](bold cyan)[$hostname](bold roninyellow) ";
-                trim_at = ".companyname.com";
-                disabled = false;
-            };
+          hostname = {
+            ssh_only = false;
+            format = "[$ssh_symbol](bold cyan)[$hostname](bold roninyellow) ";
+            trim_at = ".companyname.com";
+            disabled = false;
+          };
 
-            directory = {
-                style = "crystalblue";
-                read_only = " ";
-                truncation_length = 1;
-                truncate_to_repo = false;
-            };
+          directory = {
+            style = "crystalblue";
+            read_only = " ";
+            truncation_length = 1;
+            truncate_to_repo = false;
+          };
 
-            git_branch = {
-                symbol = " ";
-                format = "[$symbol$branch]($style) ";
-                style = "surimiorange";
-            };
+          git_branch = {
+            symbol = " ";
+            format = "[$symbol$branch]($style) ";
+            style = "surimiorange";
+          };
 
-            git_state = {
-                format = "([$state( $progress_current/$progress_total)]($style)) ";
-                style = "bright-black";
-            };
+          git_state = {
+            format = "([$state( $progress_current/$progress_total)]($style)) ";
+            style = "bright-black";
+          };
 
-            git_status = {
-                format = ''([\[$all_status$ahead_behind\]]($style) )'';
-                style = "cyan";
-            };
+          git_status = {
+            format = ''([\[$all_status$ahead_behind\]]($style) )'';
+            style = "cyan";
+          };
 
-            git_metrics.disabled = false;
+          git_metrics.disabled = false;
 
-            fill.symbol = " ";
+          fill.symbol = " ";
 
-            nodejs = {
-                format = "[$symbol($version )]($style)";
-                disabled = true;
-            };
+          nodejs = {
+            format = "[$symbol($version )]($style)";
+            disabled = true;
+          };
 
-            rust = {
-                symbol = " ";
-            };
+          rust = {
+            symbol = " ";
+          };
 
-            nix_shell = {
-                disabled = false;
-                impure_msg = "[impure shell](bold autumnred)";
-                pure_msg = "[pure shell](bold autumngreen)";
-                unknown_msg = "[unknown shell](bold roninyellow)";
-                format = ''[$symbol \[$state\]( ($name))](crystalblue) '';
-                symbol = "";
-            };
+          nix_shell = {
+            disabled = false;
+            impure_msg = "[impure shell](bold autumnred)";
+            pure_msg = "[pure shell](bold autumngreen)";
+            unknown_msg = "[unknown shell](bold roninyellow)";
+            format = ''[$symbol \[$state\]( ($name))](crystalblue) '';
+            symbol = "";
+          };
 
-            shlvl = {
-              disabled = false;
-              symbol = "󰜮";
-              style = "samuraired bold";
-            };
+          shlvl = {
+            disabled = false;
+            symbol = "󰜮";
+            style = "samuraired bold";
+          };
 
-            jobs = {
-                symbol = "";
-                style = "bold red";
-                number_threshold = 1;
-                format = "[$symbol]($style)";
-            };
+          jobs = {
+            symbol = "";
+            style = "bold red";
+            number_threshold = 1;
+            format = "[$symbol]($style)";
+          };
 
-            memory_usage = {
-                symbol = "󰍛 ";
-            };
+          memory_usage = {
+            symbol = "󰍛 ";
+          };
 
-            character = {
-                success_symbol = "[❯](purple)";
-                error_symbol = "[❯](red)";
-                vicmd_symbol = "[❮](green)";
-            };
+          character = {
+            success_symbol = "[❯](purple)";
+            error_symbol = "[❯](red)";
+            vicmd_symbol = "[❮](green)";
+          };
         };
     };
-    };
+  };
 }
