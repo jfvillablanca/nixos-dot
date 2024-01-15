6ee0588e363c6fe4fe36eb18910c3ab6b67447de
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-01-15 15:32:52 +0800

    modules: move 'internationalization' to own module

--- systems/t14g1/configuration.nix
+++ systems/t14g1/configuration.nix
@@ -11,6 +11,7 @@
       "${projectRoot}/modules/kmonad/nixos-modules.nix"
       ../../modules/steam
       ../system-modules/doas.nix
+      ../system-modules/internationalization.nix
     ];
 
   # Bootloader.
@@ -31,21 +32,6 @@
 
   # Set your time zone.
   time.timeZone = "Asia/Manila";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Enable window manager
   services = {
--- systems/virt/configuration.nix
+++ systems/virt/configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       # ./modules/kmonad-module.nix
       ../system-modules/doas.nix
+      ../system-modules/internationalization.nix
     ];
 
     # services.kmonad = {
@@ -37,21 +38,6 @@
 
   # Set your time zone.
   time.timeZone = "Asia/Manila";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Spice VDA
   services.spice-vdagentd.enable = true;
