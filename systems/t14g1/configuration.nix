b5373ac1b42ac15d2ef37525f4973037013dd356
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-01-20 18:46:19 +0800

    modules: move 'timezone' to its own module

--- systems/t14g1/configuration.nix
+++ systems/t14g1/configuration.nix
@@ -29,9 +29,6 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Set your time zone.
-  time.timeZone = "Asia/Manila";
-
   # Enable window manager
   services = {
     xserver = {
