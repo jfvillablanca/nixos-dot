25f90d4a48946de5c0038fd4d143a3ed1333bf4d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-01-23 22:11:57 +0800

    modules: move 'laptop-power-management' to its own module

---
+++ systems/system-modules/laptop-power-management.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+  # Power Management Daemon
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Values for "always plugged"
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 50;
+      # Values for "unplugged all the time"
+      # START_CHARGE_THRESH_BAT0 = 85;
+      # STOP_CHARGE_THRESH_BAT0 = 90;
+      TLP_DEFAULT_MODE = "BAT";
+      TLP_PERSISTENT_DEFAULT = 1;
+    };
+  };
+
+}
--- systems/t14g1/configuration.nix
+++ systems/t14g1/configuration.nix
@@ -16,6 +16,7 @@
       ../system-modules/networkmanager.nix
       ../system-modules/nixos-config.nix
       ../system-modules/timezone.nix
+      ../system-modules/laptop-power-management.nix
       ../system-modules/fonts.nix
     ];
 
@@ -108,20 +109,6 @@
   # Polkit (need enabled for sway)
   security.polkit.enable = true;
 
-  # Power Management Daemon 
-  services.tlp = {
-    enable = true;
-    settings = {
-      # Values for "always plugged"
-      START_CHARGE_THRESH_BAT0 = 40;
-      STOP_CHARGE_THRESH_BAT0 = 50;
-      # Values for "unplugged all the time"
-      # START_CHARGE_THRESH_BAT0 = 85;
-      # STOP_CHARGE_THRESH_BAT0 = 90;
-      TLP_DEFAULT_MODE = "BAT";
-      TLP_PERSISTENT_DEFAULT = 1;
-    };
-  };
 
 
   virtualisation.docker.enable = true;
