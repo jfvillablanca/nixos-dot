871a5db1ef6260101f5cd5031fb575d4c25d8f01
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-04-06 09:12:39 +0800

    window-manager: add 'monitors' config option

--- homeModules/window-manager/default.nix
+++ homeModules/window-manager/default.nix
@@ -21,6 +21,45 @@ in {
       type = wmType;
       default = "i3";
     };
+    monitors = lib.mkOption {
+      type = lib.types.listOf (lib.types.submodule {
+        options = {
+          name = lib.mkOption {
+            type = lib.types.str;
+            example = "DP-1";
+          };
+          isPrimary = lib.mkOption {
+            type = lib.types.bool;
+            description = "this option only works for window managers using xrandr";
+            default = false;
+          };
+          width = lib.mkOption {
+            type = lib.types.int;
+            example = 1920;
+          };
+          height = lib.mkOption {
+            type = lib.types.int;
+            example = 1080;
+          };
+          refreshRate = lib.mkOption {
+            type = lib.types.int;
+            default = 60;
+          };
+          x = lib.mkOption {
+            type = lib.types.int;
+            default = 0;
+          };
+          y = lib.mkOption {
+            type = lib.types.int;
+            default = 0;
+          };
+          enabled = lib.mkOption {
+            type = lib.types.bool;
+            default = true;
+          };
+        };
+      });
+    };
   };
   config = lib.mkMerge [
     (lib.mkIf (cfg.enable && cfg.wm == "i3") {
