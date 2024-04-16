6db6d5ad7181ab4263cdf114240e0e4f0cc5b96f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-04-16 17:52:56 +0800

    hyprland: bind audio controls

--- homeModules/hyprland/default.nix
+++ homeModules/hyprland/default.nix
@@ -171,6 +171,12 @@ in {
           ]
           ++ workspaces;
 
+        bindle = [
+          ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
+          ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ];
+
         bindm = [
           # mouse movements
           "$mainMod, mouse:272, movewindow"
