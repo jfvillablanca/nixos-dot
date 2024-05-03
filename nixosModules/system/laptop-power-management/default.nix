f6b0f2220f918c7352d232bd6271e64d7cb43e06
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-05-03 15:54:50 +0800

    t14g1: change power management values

--- nixosModules/system/laptop-power-management/default.nix
+++ nixosModules/system/laptop-power-management/default.nix
@@ -4,11 +4,11 @@
     enable = true;
     settings = {
       # Values for "always plugged"
-      START_CHARGE_THRESH_BAT0 = 40;
-      STOP_CHARGE_THRESH_BAT0 = 50;
+      # START_CHARGE_THRESH_BAT0 = 40;
+      # STOP_CHARGE_THRESH_BAT0 = 50;
       # Values for "unplugged all the time"
-      # START_CHARGE_THRESH_BAT0 = 85;
-      # STOP_CHARGE_THRESH_BAT0 = 90;
+      START_CHARGE_THRESH_BAT0 = 85;
+      STOP_CHARGE_THRESH_BAT0 = 90;
       TLP_DEFAULT_MODE = "BAT";
       TLP_PERSISTENT_DEFAULT = 1;
     };
