75214a2eb5f59fb99dda9c063b9a53937ba5d9d5
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-05-07 08:43:53 +0800

    hyprland: bind screenshot key

--- homeModules/hyprland/default.nix
+++ homeModules/hyprland/default.nix
@@ -175,6 +175,7 @@ in {
           ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
           ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ", PRINT, exec, grimblast copy area"
         ];
 
         bindm = [
