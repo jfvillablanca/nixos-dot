6abfd58dc91aca98982e4939069009bc4841e762
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-05-11 18:35:37 +0800

    wezterm: change scheme name to use colorScheme slug

--- homeModules/wezterm/default.nix
+++ homeModules/wezterm/default.nix
@@ -8,7 +8,7 @@
   # WARN: deprecation
   # https://github.com/Misterio77/nix-colors/commit/fc080c51d2a219b40d886870e364243783ed5ca1
   # https://github.com/Misterio77/nix-colors/issues/56
-  pathToScheme = builtins.toFile "wezterm-base16.yaml" (inputs.nix-colors.lib.schemeToYAML config.colorScheme);
+  pathToScheme = builtins.toFile "base16-${config.colorScheme.slug}.yaml" (inputs.nix-colors.lib.schemeToYAML config.colorScheme);
 in {
   options.myHomeModules.wezterm = {
     enable =
