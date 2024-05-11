06cbb5ff1cac7aff56afc4a795dadc3a7f81ae33
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-05-11 09:23:29 +0800

    gitui: theme with config.colorScheme
    move on from hard-coded kanagawa colors wahehe

--- homeModules/gitui/default.nix
+++ homeModules/gitui/default.nix
@@ -1,9 +1,12 @@
 {
+  inputs,
   lib,
   config,
   ...
 }: let
   cfg = config.myHomeModules.gitui;
+  inherit (config.colorScheme) palette;
+  inherit (inputs.nix-colors.lib.conversions) hexToRGBString;
 in {
   options.myHomeModules.gitui = {
     enable =
@@ -18,52 +21,27 @@ in {
         enable = true;
         theme = ''
           (
-              /* Color Palette: rebelot/kanagawa.nvim */
               selected_tab: Reset,
-              command_fg: Rgb(220, 215, 186),              // #DCD7BA (fujiWhite)
-              selection_bg: Rgb(84, 84, 109),              // #54546D (sumiInk4)
+              command_fg: Rgb(${hexToRGBString "," palette.base05}),
+              selection_bg: Rgb(${hexToRGBString "," palette.base02}),
               selection_fg: White,
-              cmdbar_bg: Rgb(84, 84, 109),                 // #54546D (sumiInk4)
-              cmdbar_extra_lines_bg: Rgb(84, 84, 109),     // #54546D (sumiInk4)
-              disabled_fg: Rgb(114, 113, 105),             // #727169 (fujiGray)
+              cmdbar_bg: Rgb(${hexToRGBString "," palette.base02}),
+              cmdbar_extra_lines_bg: Rgb(${hexToRGBString "," palette.base02}),
+              disabled_fg: Rgb(${hexToRGBString "," palette.base04}),
               diff_line_add: Green,
               diff_line_delete: Red,
               diff_file_added: LightGreen,
               diff_file_removed: LightRed,
               diff_file_moved: LightMagenta,
               diff_file_modified: Yellow,
-              commit_hash: Rgb(210, 126, 153),             // #D27E99 (sakuraPink)
-              commit_time: Rgb(127, 180, 202),             // #7FB4CA (springBlue)
-              commit_author: Rgb(152, 187, 108),           // #98BB6C (springGreen)
+              commit_hash: Rgb(${hexToRGBString "," palette.base0F}),
+              commit_time: Rgb(${hexToRGBString "," palette.base0D}),
+              commit_author: Rgb(${hexToRGBString "," palette.base0E}),
               danger_fg: Red,
               push_gauge_bg: Blue,
               push_gauge_fg: Reset,
               tag_fg: LightMagenta,
-              branch_fg: Rgb(230, 195, 132),               // #E6C384 (carpYellow)
-
-              /* Default Colors */
-
-              /* selected_tab: Reset, */
-              /* command_fg: White, */
-              /* selection_bg: Blue, */
-              /* selection_fg: White, */
-              /* cmdbar_bg: Blue, */
-              /* cmdbar_extra_lines_bg: Blue, */
-              /* disabled_fg: DarkGray, */
-              /* diff_line_add: Green, */
-              /* diff_line_delete: Red, */
-              /* diff_file_added: LightGreen, */
-              /* diff_file_removed: LightRed, */
-              /* diff_file_moved: LightMagenta, */
-              /* diff_file_modified: Yellow, */
-              /* commit_hash: Magenta, */
-              /* commit_time: LightCyan, */
-              /* commit_author: Green, */
-              /* danger_fg: Red, */
-              /* push_gauge_bg: Blue, */
-              /* push_gauge_fg: Reset, */
-              /* tag_fg: LightMagenta, */
-              /* branch_fg: LightYellow, */
+              branch_fg: Rgb(${hexToRGBString "," palette.base0A}),
           )
         '';
       };
