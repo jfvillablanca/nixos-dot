58c55cbf30fef754d10867ca11755d864fc24392
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-05-13 18:40:18 +0800

    nvim(octo-nvim): change approve_review bind
    conflicts with the tmux command prefix lmao

--- homeModules/neovim/lua/octo-nvim.lua
+++ homeModules/neovim/lua/octo-nvim.lua
@@ -190,7 +190,7 @@ octo.setup({
             react_confused = { lhs = "gOrc", desc = "add/remove 😕 reaction" },
         },
         submit_win = {
-            approve_review = { lhs = "<C-a>", desc = "approve review" },
+            approve_review = { lhs = "<C-o>", desc = "approve review" },
             comment_review = { lhs = "<C-m>", desc = "comment review" },
             request_changes = { lhs = "<C-r>", desc = "request changes review" },
             close_review_tab = { lhs = "<C-c>", desc = "close review tab" },
