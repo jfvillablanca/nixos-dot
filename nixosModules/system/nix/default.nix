8eab1020ae4759c641d044bb3fef5de436be0ef1
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-05-25 21:23:48 +0800

    nix(substituters): comment out walker cachix

--- nixosModules/system/nix/default.nix
+++ nixosModules/system/nix/default.nix
@@ -9,11 +9,11 @@
       auto-optimise-store = true;
       substituters = [
         "https://hyprland.cachix.org"
-        "https://walker.cachix.org"
+        # "https://walker.cachix.org"
       ];
       trusted-public-keys = [
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+        # "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
       ];
       experimental-features = ["nix-command" "flakes"];
     };
