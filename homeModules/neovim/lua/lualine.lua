7cc6ae0ed3f3b966f0816cabfde084ce28ff2f99
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-06-21 13:38:07 +0800

    lualine: fix deprecated lsp method

--- homeModules/neovim/lua/lualine.lua
+++ homeModules/neovim/lua/lualine.lua
@@ -67,7 +67,7 @@ local lspserver = {
 	function()
 		local msg = "No Active Lsp"
 		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-		local clients = vim.lsp.get_active_clients()
+		local clients = vim.lsp.get_clients()
 		if next(clients) == nil then
 			return msg
 		end
