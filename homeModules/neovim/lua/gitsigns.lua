36e1a3dcb92237d61791c4c6e68b7f7390fefe6d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-06-27 09:56:28 +0800

    gitsigns: update due to config deprecation

--- homeModules/neovim/lua/gitsigns.lua
+++ homeModules/neovim/lua/gitsigns.lua
@@ -5,36 +5,11 @@ end
 
 gitsigns.setup({
 	signs = {
-		add = {
-			hl = "GitSignsAdd",
-			text = "▎",
-			numhl = "GitSignsAddNr",
-			linehl = "GitSignsAddLn",
-		},
-		change = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
-		delete = {
-			hl = "GitSignsDelete",
-			text = "󰐊",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		topdelete = {
-			hl = "GitSignsDelete",
-			text = "󰐊",
-			numhl = "GitSignsDeleteNr",
-			linehl = "GitSignsDeleteLn",
-		},
-		changedelete = {
-			hl = "GitSignsChange",
-			text = "▎",
-			numhl = "GitSignsChangeNr",
-			linehl = "GitSignsChangeLn",
-		},
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "󰐊" },
+		topdelete = { text = "󰐊" },
+		changedelete = { text = "▎" },
 	},
 	signcolumn = true,                      -- Toggle with `:Gitsigns toggle_signs`
 	numhl = false,                          -- Toggle with `:Gitsigns toggle_numhl`
@@ -52,9 +27,7 @@ gitsigns.setup({
 		delay = 1000,
 		ignore_whitespace = false,
 	},
-	current_line_blame_formatter_opts = {
-		relative_time = false,
-	},
+    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil,                 -- Use default
