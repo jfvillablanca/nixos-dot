511b25d8e12456491ddf8c1360c2787c95390631
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-08-31 08:15:26 +0800

    whichkey: rewrite config due to v3 changes
    - will rewrite this to reduce duplication. this is just based on the
      generated migration suggestion from whichkey themself

--- homeModules/neovim/lua/whichkey.lua
+++ homeModules/neovim/lua/whichkey.lua
@@ -26,28 +26,10 @@ local setup = {
 	-- add operators that will trigger motion and text object completion
 	-- to enable all native operators, set the preset / operators plugin above
 	-- operators = { gc = "Comments" },
-	key_labels = {
-		-- override the label used to display some keys. It doesn't effect WK in any other way.
-		-- For example:
-		-- ["<space>"] = "SPC",
-		-- ["<cr>"] = "RET",
-		-- ["<tab>"] = "TAB",
-	},
 	icons = {
 		breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
 		separator = "➜", -- symbol used between a key and it's label
 		group = "+", -- symbol prepended to a group
-	},
-	popup_mappings = {
-		scroll_down = "<C-d>", -- binding to scroll down inside the popup
-		scroll_up = "<C-u>", -- binding to scroll up inside the popup
-	},
-	window = {
-		border = "rounded", -- none, single, double, shadow
-		position = "bottom", -- bottom, top
-		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-		winblend = 0,
 	},
 	layout = {
 		height = { min = 4, max = 25 }, -- min and max height of the columns
@@ -55,147 +37,83 @@ local setup = {
 		spacing = 3, -- spacing between columns
 		align = "left", -- align columns left, center or right
 	},
-	ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
-	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 	show_help = true, -- show help message on the command line when the popup is visible
-	triggers = "auto", -- automatically setup triggers
-	-- triggers = {"<leader>"} -- or specify a list manually
-	triggers_blacklist = {
-		-- list of mode / prefixes that should never be hooked by WhichKey
-		-- this is mostly relevant for key maps that start with a native binding
-		-- most people should not need to change this
-		-- i = { "j", "k" },
-		-- v = { "j", "k" },
-	},
 }
 
 local opts = {
-	mode = "n", -- NORMAL mode
-	prefix = "<leader>",
-	buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-	silent = true, -- use `silent` when creating keymaps
-	noremap = true, -- use `noremap` when creating keymaps
-	nowait = true, -- use `nowait` when creating keymaps
+	mode = { "n" }, -- NORMAL mode
 }
 
-local general = {
-	["w"] = { "<cmd>w!<CR>", "Save" },
-	["q"] = { "<cmd>q!<CR>", "Quit" },
-	["c"] = { "<cmd>bd!<CR>", "Close Buffer" },
-	["T"] = { "<cmd>set foldmethod=expr<cr>", "Treesitter foldmethod" },
-}
+wk.add({
+   {"<leader>w",  "<cmd>w!<CR>", desc = "Save" },
+   {"<leader>q",  "<cmd>q!<CR>", desc = "Quit" },
+   {"<leader>c",  "<cmd>bd!<CR>", desc = "Close Buffer" },
+   {"<leader>T",  "<cmd>set foldmethod=expr<cr>", desc = "Treesitter foldmethod" },
+}, opts)
 
-local nvimtree = {
-	n = {
-		name = "NvimTree",
-		e = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
-	},
-}
+wk.add({
+    { "<leader>n", group = "NvimTree" },
+    { "<leader>ne", "<cmd>NvimTreeToggle<cr>", desc = "Explorer" },
+}, opts)
 
-local git = {
-	g = {
-		name = "Git",
-        s = {
-            name = "Gitsigns",
-            n = { "<cmd>Gitsigns next_hunk<cr>", "Next Hunk" },
-            e = { "<cmd>Gitsigns prev_hunk<cr>", "Prev Hunk" },
-            l = { "<cmd>Gitsigns blame_line<cr>", "Blame" },
-            p = { "<cmd>Gitsigns preview_hunk<cr>", "Preview Hunk" },
-            r = { "<cmd>Gitsigns reset_hunk<cr>", "Reset Hunk" },
-            R = { "<cmd>Gitsigns reset_buffer<cr>", "Reset Buffer" },
-            s = { "<cmd>Gitsigns stage_hunk<cr>", "Stage Hunk" },
-            u = {
-                "<cmd>Gitsigns undo_stage_hunk<cr>",
-                "Undo Stage Hunk",
-            },
-        },
-		t = { "<cmd>diffget //2<cr>", "Diffget from target branch" },
-		n = { "<cmd>diffget //3<cr>", "Diffget from merge branch" },
-	},
-}
 
-local telescope = {
-	t = {
-		name = "Telescope",
-		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-		f = {
-			"<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-			"Find files",
-		},
-		h = {
-			"<cmd>lua require('telescope.builtin').help_tags(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-			"Find help",
-		},
-		k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-		r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-		w = {
-			"<cmd>Telescope grep_string<cr>",
-			"Search current word",
-		},
-		R = { "<cmd>Telescope registers<cr>", "Registers" },
-		C = { "<cmd>Telescope commands<cr>", "Commands" },
-	},
-	["<space>"] = { "<cmd>Telescope buffers<cr>", "Buffers", },
-	["/"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-}
+wk.add({
+    { "<leader>g", group = "Git", nowait = true, remap = false },
+    { "<leader>gn", "<cmd>diffget //3<cr>", desc = "Diffget from merge branch", nowait = true, remap = false },
+    { "<leader>gs", group = "Gitsigns", nowait = true, remap = false },
+    { "<leader>gsR", "<cmd>Gitsigns reset_buffer<cr>", desc = "Reset Buffer", nowait = true, remap = false },
+    { "<leader>gse", "<cmd>Gitsigns prev_hunk<cr>", desc = "Prev Hunk", nowait = true, remap = false },
+    { "<leader>gsl", "<cmd>Gitsigns blame_line<cr>", desc = "Blame", nowait = true, remap = false },
+    { "<leader>gsn", "<cmd>Gitsigns next_hunk<cr>", desc = "Next Hunk", nowait = true, remap = false },
+    { "<leader>gsp", "<cmd>Gitsigns preview_hunk<cr>", desc = "Preview Hunk", nowait = true, remap = false },
+    { "<leader>gsr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Reset Hunk", nowait = true, remap = false },
+    { "<leader>gss", "<cmd>Gitsigns stage_hunk<cr>", desc = "Stage Hunk", nowait = true, remap = false },
+    { "<leader>gsu", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "Undo Stage Hunk", nowait = true, remap = false },
+    { "<leader>gt", "<cmd>diffget //2<cr>", desc = "Diffget from target branch", nowait = true, remap = false },
+}, opts)
 
-local treesj = {
-	m = {
-		name = "TreeSJ",
-		t = { "<cmd>TSJToggle<cr>", "TreeSJ Toggle" },
-	},
-}
+wk.add({
+    { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find Text", nowait = true, remap = false },
+    { "<leader><space>", "<cmd>Telescope buffers<cr>", desc = "Buffers", nowait = true, remap = false },
+    { "<leader>t", group = "Telescope", nowait = true, remap = false },
+    { "<leader>tC", "<cmd>Telescope commands<cr>", desc = "Commands", nowait = true, remap = false },
+    { "<leader>tR", "<cmd>Telescope registers<cr>", desc = "Registers", nowait = true, remap = false },
+    { "<leader>tb", "<cmd>Telescope git_branches<cr>", desc = "Checkout branch", nowait = true, remap = false },
+    { "<leader>tc", "<cmd>Telescope git_commits<cr>", desc = "Checkout commit", nowait = true, remap = false },
+    { "<leader>tf", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>", desc = "Find files", nowait = true, remap = false },
+    { "<leader>th", "<cmd>lua require('telescope.builtin').help_tags(require('telescope.themes').get_dropdown{previewer = false})<cr>", desc = "Find help", nowait = true, remap = false },
+    { "<leader>tk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps", nowait = true, remap = false },
+    { "<leader>to", "<cmd>Telescope git_status<cr>", desc = "Open changed file", nowait = true, remap = false },
+    { "<leader>tr", "<cmd>Telescope oldfiles<cr>", desc = "Open Recent File", nowait = true, remap = false },
+    { "<leader>tw", "<cmd>Telescope grep_string<cr>", desc = "Search current word", nowait = true, remap = false },
+}, opts)
 
-local lsp = {
-	["<leader>l"] = {
-		name = "LSP",
-		a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-		d = {
-			"<cmd>Telescope diagnostics bufnr=0<cr>",
-            "Document Diagnostics",
-        },
-		w = {
-			"<cmd>Telescope diagnostics<cr>",
-			"Workspace Diagnostics",
-		},
-		f = { "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", "Format" },
-		i = { "<cmd>LspInfo<cr>", "Info" },
-		l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
-		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-		S = {
-			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-			"Workspace Symbols",
-		},
-	},
-	["<leader>m"] = {
-		name = "Trouble",
-		x = { "<cmd>Trouble<cr>", "Toggle" },
-		d = { "<cmd>Trouble diagnostics<cr>", "Diagnostics" },
-		l = { "<cmd>Trouble loclist<cr>", "Loclist" },
-		q = { "<cmd>Trouble quickfix<cr>", "Quickfix" },
-	},
-	g = {
-		d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "LSP Definition" },
-		D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "LSP Declaration" },
-		I = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "LSP Implementation" },
-		r = { "<cmd>Trouble lsp_references<cr>", "LSP References" },
-	},
-	["K"] = { "<cmd>lua vim.lsp.buf.hover()<CR>", "LSP Hover" },
-}
+wk.add({
+    { "<leader>m", group = "TreeSJ", nowait = true, remap = false },
+    { "<leader>mt", "<cmd>TSJToggle<cr>", desc = "TreeSJ Toggle", nowait = true, remap = false },
+}, opts)
+
+wk.add({
+    { "<leader>l", group = "LSP", nowait = true, remap = false },
+    { "<leader>lS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "Workspace Symbols", nowait = true, remap = false },
+    { "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", desc = "Code Action", nowait = true, remap = false },
+    { "<leader>ld", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics", nowait = true, remap = false },
+    { "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", desc = "Format", nowait = true, remap = false },
+    { "<leader>li", "<cmd>LspInfo<cr>", desc = "Info", nowait = true, remap = false },
+    { "<leader>ll", "<cmd>lua vim.lsp.codelens.run()<cr>", desc = "CodeLens Action", nowait = true, remap = false },
+    { "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", desc = "Rename", nowait = true, remap = false },
+    { "<leader>ls", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols", nowait = true, remap = false },
+    { "<leader>lw", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics", nowait = true, remap = false },
+    { "<leader>m", group = "Trouble", nowait = true, remap = false },
+    { "<leader>md", "<cmd>Trouble diagnostics<cr>", desc = "Diagnostics", nowait = true, remap = false },
+    { "<leader>ml", "<cmd>Trouble loclist<cr>", desc = "Loclist", nowait = true, remap = false },
+    { "<leader>mq", "<cmd>Trouble quickfix<cr>", desc = "Quickfix", nowait = true, remap = false },
+    { "<leader>mx", "<cmd>Trouble<cr>", desc = "Toggle", nowait = true, remap = false },
+    { "K", "<cmd>lua vim.lsp.buf.hover()<CR>", desc = "LSP Hover", nowait = true, remap = false },
+    { "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", desc = "LSP Declaration", nowait = true, remap = false },
+    { "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", desc = "LSP Implementation", nowait = true, remap = false },
+    { "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", desc = "LSP Definition", nowait = true, remap = false },
+    { "gr", "<cmd>Trouble lsp_references<cr>", desc = "LSP References", nowait = true, remap = false },
+}, opts)
 
 wk.setup(setup)
-wk.register(general, opts)
-wk.register(nvimtree, opts)
-wk.register(git, opts)
-wk.register(telescope, opts)
-wk.register(treesj, opts)
-wk.register(lsp, {
-	mode = "n",
-	buffer = nil,
-	silent = true,
-	noremap = true,
-	nowait = true,
-})
