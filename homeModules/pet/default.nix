20d227fa91f3593496d0ae36acf6718e2a075b4e
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-11-01 10:34:58 +0800

    pet: add snippets

--- homeModules/pet/default.nix
+++ homeModules/pet/default.nix
@@ -11,7 +11,53 @@ in {
   config = lib.mkIf cfg.enable {
     programs.pet = {
       enable = true;
-      snippets = [];
+      snippets = [
+        {
+          description = "Nix store garbage collect";
+          command = "nix-store --gc";
+          tag = ["nix"];
+        }
+        {
+          description = "Fetch nix info";
+          command = "nix-info --run 'nix-info -m'";
+          tag = ["nix"];
+        }
+        {
+          description = "Nix fmt with alejandra";
+          command = "alejandra <files/directories>";
+          tag = ["nix"];
+        }
+        {
+          description = "Update package in a flake";
+          command = "nix-update --version=<version> <package-name>";
+          tag = ["nix"];
+        }
+        {
+          description = "Update a singular flake input";
+          command = "nix flake lock --update-input <input-name>";
+          tag = ["nix"];
+        }
+        {
+          description = "Get commit count of current branch";
+          command = "git rev-list --count HEAD";
+          tag = ["git"];
+        }
+        {
+          description = "Fuzzy search git log";
+          command = "git log --oneline | fzf --ansi --preview \"git show --color=always {1}\"";
+          tag = ["git"];
+        }
+        {
+          description = "Format Purescript in-place with purs-tidy";
+          command = "purs-tidy format-in-place <glob>";
+          tag = ["purescript"];
+        }
+        {
+          description = "Format Just in-place with just fmt";
+          command = "just --fmt --unstable";
+          tag = ["just"];
+        }
+      ];
     };
   };
 }
