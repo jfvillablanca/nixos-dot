caeccc34644ffe8a42a221e646d17ef6ebc9b0e1
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2024-11-07 13:49:15 +0800

    wezterm: annotate code fence

--- homeModules/wezterm/default.nix
+++ homeModules/wezterm/default.nix
@@ -16,21 +16,25 @@ in {
     programs = {
       wezterm = {
         enable = true;
-        extraConfig = ''
-          return {
-            -- NOTE: workaround due to text rendering as color blocks
-            -- https://github.com/wez/wezterm/issues/5990
-            front_end = "WebGpu",
+        extraConfig =
+          /*
+          lua
+          */
+          ''
+            return {
+              -- NOTE: workaround due to text rendering as color blocks
+              -- https://github.com/wez/wezterm/issues/5990
+              front_end = "WebGpu",
 
-            enable_tab_bar = false,
+              enable_tab_bar = false,
 
-            enable_wayland = false,
+              enable_wayland = false,
 
-            window_close_confirmation = 'NeverPrompt',
+              window_close_confirmation = 'NeverPrompt',
 
-            warn_about_missing_glyphs = false,
-          }
-        '';
+              warn_about_missing_glyphs = false,
+            }
+          '';
       };
     };
   };
