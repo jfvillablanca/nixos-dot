588624bf42963a86fa99395713e1d617bd63ed7d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-01-19 23:16:57 +0800

    wezterm: expose frontEnd and enableWayland options

--- homeModules/wezterm/default.nix
+++ homeModules/wezterm/default.nix
@@ -6,12 +6,22 @@
   cfg = config.myHomeModules.wezterm;
 in {
   options.myHomeModules.wezterm = {
-    enable =
-      lib.mkEnableOption "enables wezterm"
-      // {
-        default = false;
-      };
+    enable = lib.mkEnableOption "enables wezterm";
+
+    frontEnd = lib.mkOption {
+      type = lib.types.enum ["WebGpu" "OpenGL"];
+      default = "WebGpu";
+      description = "Select the front end for wezterm. Options are 'WebGpu' or 'OpenGL'.";
+      example = "OpenGL";
+    };
+
+    enableWayland = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable Wayland support in wezterm.";
+    };
   };
+
   config = lib.mkIf cfg.enable {
     programs = {
       wezterm = {
@@ -25,7 +35,7 @@ in {
               -- NOTE: workaround due to text rendering as color blocks
               -- https://github.com/wez/wezterm/issues/5990
               -- front_end = "WebGpu",
-              front_end = "OpenGL",
+              front_end = "${cfg.frontEnd}",
 
               enable_tab_bar = true,
 
