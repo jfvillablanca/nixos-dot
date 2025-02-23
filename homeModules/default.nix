f4b7a6ec19e08b48258a5278a38bde6be71d2e0d
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-02-23 23:29:47 +0800

    kitty: add kitty with cursor trail 🐱

--- homeModules/default.nix
+++ homeModules/default.nix
@@ -25,6 +25,7 @@
     ./neovim
     ./yazi
     ./pet
+    ./kitty
 
     ./window-manager
     ./i3
---
+++ homeModules/kitty/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.myHomeModules.kitty;
+in {
+  options.myHomeModules.kitty = {
+    enable = lib.mkEnableOption "enables kitty";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs = {
+      kitty = {
+        enable = true;
+        settings = {
+          cursor_trail = 1;
+          cursor_trail_decay = "0.05 0.2";
+          cursor_shape = "block";
+          cursor_trail_start_threshold = 0;
+          shell_integration = "no-cursor";
+        };
+      };
+    };
+  };
+}
