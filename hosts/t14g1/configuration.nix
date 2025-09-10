484e8fef88e40c93c884731a72c664623922a62b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2025-09-11 02:59:20 +0800

    t14g1: fix font breaking change

--- hosts/t14g1/configuration.nix
+++ hosts/t14g1/configuration.nix
@@ -37,7 +37,7 @@
     };
     fonts = {
       monospace = {
-        package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+        package = pkgs.nerd-fonts.jetbrains-mono;
         name = "JetBrainsMono Nerd Font Mono";
       };
       sansSerif = {
