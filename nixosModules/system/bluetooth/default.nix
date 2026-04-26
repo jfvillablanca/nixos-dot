ee5afc4673e2b140afb8ef9910ee084b08e81a35
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2026-04-27 05:56:39 +0800

    bluetooth: pin to pkgs-stable-24-05

--- nixosModules/system/bluetooth/default.nix
+++ nixosModules/system/bluetooth/default.nix
@@ -1,11 +1,16 @@
-{...}: {
+{pkgs-stable-24-05, ...}: {
   hardware.bluetooth = {
     enable = true;
+    package = pkgs-stable-24-05.bluez;
     powerOnBoot = true;
     settings = {
       General = {
         Experimental = false;
+        # FastConnectable = true;
       };
+      # Policy = {
+      #   AutoEnable = true;
+      # };
     };
   };
   # NOTE: exec-once 'blueman-applet' in your respective window manager
