60eb4576d89c447883048e327c764e67c6b676cb
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2026-05-05 01:24:57 +0800

    flake/packages: cimmerian/t14g1 enable nixd NixOS options expansion

--- modules/flake/packages.nix
+++ modules/flake/packages.nix
@@ -16,6 +16,10 @@
   flake.packages.x86_64-linux = {
     nvim = inputs.self.nixosConfigurations.cimmerian.config.home-manager.users.jmfv.programs.neovim.finalPackage;
 
+    # nixd's NixOS options expansion is host-specific (it embeds the flake's
+    # local checkout path and the host name). Lives at the factory call site
+    # rather than the server module so the standalone `.#nvim-experimental`
+    # doesn't bake in any user's flake path.
     nvim-experimental-cimmerian = self.factory.nvim {
       system = "x86_64-linux";
       colorscheme = "base16-spaceduck";
@@ -26,6 +30,10 @@
         {
           nvim.lsp.servers.eslint.enable = true;
           nvim.lsp.servers.texlab.enable = true;
+          nvim.lsp.servers.nixd.settings.nixd.options = {
+            nixos.expr = ''(builtins.getFlake "/home/jmfv/nixos-dot").nixosConfigurations."cimmerian".options'';
+            home_manager.expr = ''(builtins.getFlake "/home/jmfv/nixos-dot").nixosConfigurations."cimmerian".config.home-manager.users.jmfv'';
+          };
         }
       ];
     };
@@ -40,6 +48,10 @@
         {
           nvim.lsp.servers.eslint.enable = true;
           nvim.lsp.servers.texlab.enable = true;
+          nvim.lsp.servers.nixd.settings.nixd.options = {
+            nixos.expr = ''(builtins.getFlake "/home/jmfv/nixos-dot").nixosConfigurations."t14g1".options'';
+            home_manager.expr = ''(builtins.getFlake "/home/jmfv/nixos-dot").nixosConfigurations."t14g1".config.home-manager.users.jmfv'';
+          };
         }
       ];
     };
