cb5297ffa402b3496a82bc0c9ac406d23a41d0d4
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2026-06-03 13:42:05 -0700

    system/timezone: add darwin twin (declarative tz via systemsetup)

--- modules/system/timezone/default.nix
+++ modules/system/timezone/default.nix
@@ -1,9 +1,15 @@
-{
+let
+  timeZone = "Asia/Manila";
+in {
   flake.modules.nixos.timezone = _: {
-    time.timeZone = "Asia/Manila";
+    time.timeZone = timeZone;
     services.chrony = {
       enable = true;
       servers = ["time.cloudflare.com"];
     };
+  };
+
+  flake.modules.darwin.timezone = _: {
+    time.timeZone = timeZone;
   };
 }
