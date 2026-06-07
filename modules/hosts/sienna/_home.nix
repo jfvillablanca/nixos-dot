c7e05682660c4ad94673f21be5eff3bdf27a599a
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2026-06-08 01:18:49 +0800

    hosts/sienna: silence gtk4.theme eval warning (pin legacy default)

--- modules/hosts/sienna/_home.nix
+++ modules/hosts/sienna/_home.nix
@@ -40,6 +40,8 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/${base16Scheme}.yaml";
   };
 
+  gtk.gtk4.theme = config.gtk.theme;
+
   programs.moonlight.extraSettings = {
     General = {
       width = 3840;
