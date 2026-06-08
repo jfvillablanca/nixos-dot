f584ddb85345ef1e97b88b09329493d2eaa339fe
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2026-06-09 00:51:36 +0800

    kitty: send macOS Option as Alt/Meta for tmux M- parity

--- modules/programs/kitty/default.nix
+++ modules/programs/kitty/default.nix
@@ -1,19 +1,30 @@
 {
-  flake.modules.homeManager.kitty = _: {
+  flake.modules.homeManager.kitty = {
+    lib,
+    pkgs,
+    ...
+  }: {
     config = {
       programs = {
         kitty = {
           enable = true;
-          settings = {
-            window_padding_width = 5;
-            confirm_os_window_close = 0;
+          settings = lib.mkMerge [
+            {
+              window_padding_width = 5;
+              confirm_os_window_close = 0;
 
-            cursor_trail = 1;
-            cursor_trail_decay = "0.05 0.2";
-            cursor_shape = "block";
-            cursor_trail_start_threshold = 0;
-            shell_integration = "no-cursor";
-          };
+              cursor_trail = 1;
+              cursor_trail_decay = "0.05 0.2";
+              cursor_shape = "block";
+              cursor_trail_start_threshold = 0;
+              shell_integration = "no-cursor";
+            }
+            # macOS: send Option as Alt/Meta so tmux M- bindings (pane/window
+            # nav) behave exactly as on Linux.
+            (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
+              macos_option_as_alt = "yes";
+            })
+          ];
         };
       };
     };
